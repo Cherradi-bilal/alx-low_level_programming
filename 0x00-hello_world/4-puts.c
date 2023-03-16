59837a3dff44f6ecbe00c319d1ff29688caa205e
Cherradi-bilal <bilalcherradi36@yahoo.com>
2023-03-16 14:55:36 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main -  A program that prints a line with puts function
+ *  Return : 0 (Success)
+ */
+int main(viod)
+{
+pust("\"Progtamming is like building a multiligual puzz le ");
+return (0);
+}
