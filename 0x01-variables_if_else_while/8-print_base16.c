1087fa30586fffe4b816a1e7d32f9b3b56d6d68a
Cherradi-bilal <bilalcherradi36@yahoo.com>
2023-03-18 09:18:56 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Printf if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+    int i;
+
+    for (i = 48; i <= 57; i++)
+        putchar(i);
+
+    for (i = 97; i <= 102; i++)
+        putchar(i);
+
+    putchar('\n');
+
+    return (0);
+}
+
