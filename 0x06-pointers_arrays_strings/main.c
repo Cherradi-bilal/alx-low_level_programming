d44f11d5d4390856bd751629e1dd5a3ae72471b9
Cherradi-bilal <bilalcherradi36@yahoo.com>
2023-03-30 11:31:26 -0700

    all tasks

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -14,4 +15,6 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
 #endif
+
