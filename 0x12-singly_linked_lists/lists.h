582d69850093c17c95a28905eff0f0c2882c5db8
cherradi-bilal <bilalcherradi36@yahoo.com>
2023-04-27 12:46:39 -0700

    all tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,25 +1,27 @@
-#ifndef MY_LISTS_H
-#define MY_LISTS_H
+[200~#ifndef LISTS_H
+#define LISTS_H
 
 /**
-
-struct my_list_s - singly linked list
-@my_str: string - (malloc'ed string)
-@my_len: length of the string
-@my_next: points to the next node
-Description: singly linked list node structure
-for my project
-*/
-typedef struct my_list_s
+ *  * struct list_s - singly linked list
+ *   * @str: string - (malloc'ed string)
+ *    * @len: length of the string
+ *     * @next: points to the next node
+ *      *
+ *       * Description: singly linked list node structure
+ *        * for Holberton project
+ *         */
+typedef struct list_s
 {
-char *my_str;
-unsigned int my_len;
-struct my_list_s *my_next;
-} my_list_t;
-size_t print_my_list(const my_list_t *my_head);
-size_t my_list_len(const my_list_t *my_head);
-my_list_t *add_my_node(my_list_t **my_head, const char *my_str);
-my_list_t *add_my_node_end(my_list_t **my_head, const char *my_str);
-void free_my_list(my_list_t *my_head);
+		char *str;
+			unsigned int len;
+				struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
+
